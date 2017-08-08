8e5f5ac2aab4f0fe9e68bfe5ce332df76e8b7d9f
Richard <r.scheglmann@gmail.com>
2017-08-08 22:08:03 +0200

    removed sensible data

---
+++ classes/myFtpUploader-example.py
@@ -0,0 +1,25 @@
+#
+# myFtpUploader.py Class - to upload the generated html files to our webserver
+#
+
+import ftplib
+
+# Class START
+class myFtpUploader:
+
+    tempFileName = ""
+
+    def __init__(self, directory, _tempFileName):
+        server = 'server'
+        username = 'username'
+        password = 'password'
+        ftp_connection = ftplib.FTP(server, username, password)
+
+
+        remote_path = directory
+        ftp_connection.cwd(remote_path)
+        #session = ftplib.FTP('example.com','username','password')
+        file = open('E:/files/OneDrive/Programming/Python Programming/Untis-Vertretungsplan-htmlParser-and-uploader/tempUntisData/Lehrer heute/LehrerAllHeute.html', 'rb') # file to send
+        ftp_connection.storbinary('STOR '+_tempFileName, file)     # send the file
+        file.close()                                    # close file and FTP
+        ftp_connection.quit()
